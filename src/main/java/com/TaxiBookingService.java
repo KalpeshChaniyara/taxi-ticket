a5b2b99ee1f19eb52622cf8b7239169154e12f73
KalpeshChaniyara <kalpeshkumar.chaniyara@oracle.com>
2021-01-07 01:07:17 +0530

    adding api documentation

--- src/main/java/com/TaxiBookingService.java
+++ src/main/java/com/TaxiBookingService.java
@@ -1,5 +1,12 @@
 package com;
 
 public interface TaxiBookingService {
+    /**
+     *
+     * @param source source city
+     * @param destination destination city
+     * @param noOfTravellers no of travellers
+     * @return ticket
+     */
     String bookTaxi(String source, String destination, int noOfTravellers);
 }
